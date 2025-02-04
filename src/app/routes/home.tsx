b8e9466c1fd5e283c7c9f3e2f0cf6c22dcaa4602
Nikita Yakunin <nayakunin99@gmail.com>
2025-02-04 13:15:45 +0100

    first prototype

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { getCuratedPhotos } from "src/api";
+import { getCuratedPhotos } from "@/api";
 
 import type { Route } from "./+types/home";
 
@@ -10,9 +10,22 @@ export function meta() {
 }
 
 export async function loader() {
-  return await getCuratedPhotos();
+  return await getCuratedPhotos({
+    per_page: 24,
+  });
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <div>{JSON.stringify(loaderData)}</div>;
+  return (
+    <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-2">
+      {loaderData.photos.map((photo) => (
+        <img
+          key={photo.id}
+          src={photo.src.large2x}
+          alt={photo.alt}
+          className="w-full mb-2"
+        />
+      ))}
+    </div>
+  );
 }
