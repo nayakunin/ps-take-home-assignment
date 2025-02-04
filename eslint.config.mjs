635481aa1f001b618634775f70246c068ba8c85d
Nikita Yakunin <nayakunin99@gmail.com>
2025-02-04 12:39:01 +0100

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+const baseConfig = tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: ["**/+types/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-namespace": "off",
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+);
+
+export default baseConfig;
